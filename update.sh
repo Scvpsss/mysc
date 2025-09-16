7b1d9c66da59cdeb4b3d1167dfdb641f2e3d263c
Scvpsss <158008928+Scvpsss@users.noreply.github.com>
2025-09-16 23:55:51 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/BayuXD-Tunnel/mysc/main/limit/menu.zip
+    wget https://raw.githubusercontent.com/Scvpsss/mysc/main/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
